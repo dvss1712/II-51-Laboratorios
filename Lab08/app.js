6506819a934e54cfdf2cd8cfaa7d31badb88eca5
David <dv1712@gmail.com>
2025-11-19 21:50:38 -0600

    lab08 final

--- Lab08/app.js
+++ Lab08/app.js
@@ -1,5 +1,4 @@
 import { supabase } from './supabaseClient.js';
-
 const form = document.getElementById('curso-form');
 const inputid = document.getElementById('id');
 const inputcodigo = document.getElementById('codigo');
@@ -8,7 +7,8 @@ const inputcreditos = document.getElementById('creditos');
 const btnSave = document.getElementById("btn-save");
 const btnCancel = document.getElementById("btn-cancel");
 const statusDiv = document.getElementById("status");
- 
+let editando = false; 
+let listaCursos = document.getElementById("lista");
 // 
 // eventos
 //=========================
@@ -18,8 +18,16 @@ form.addEventListener('submit', async (e) => {
     const nombre = inputnombre.value.trim();
     const creditos = parseInt(inputcreditos.value);
     if (editando) {}
-    else {await crearcurso(codigo, nombre, creditos);}
-   
+    else {await crearcursos(codigo, nombre, creditos);}
+    form.reset();
+
+});
+listaCursos.addEventListener("click", async (e) => {
+    if (e.target.classList.contains("btn-delete")) {
+        const id = e.target.getAttribute("data-id");
+        await eliminarcursos(id);
+        cargarcursos();
+     }
 });
 
 async function cargarcursos() {
@@ -30,16 +38,30 @@ async function cargarcursos() {
         console.error("Error al cargar cursos:", error);
         return;
     }
-    let listaCursos = document.getElementById('lista');
-    listaCursos.innerHTML = '';
+
+   listaCursos.innerHTML = "";
     cursos.forEach(curso => {
-        let li = document.createElement('li');
-        li.textContent = `${curso.codigo} - ${curso.nombre} [${curso.creditos} créditos]`;
-        lista.appendChild(li);
+        let li = document.createElement("li");
+        //li.textContent = curso.codigo + " - " + curso.nombre;
+        li.innerHTML = `${curso.codigo} - ${curso.nombre} [${curso.creditos} Creditos] <button class="btn-delete" data-id="${curso.id}">Eliminar</button>`;
+        listaCursos.appendChild(li);
     });
 }
-async function crearcurso(codigo, nombre, creditos) {
+
+async function crearcursos(codigo, nombre, creditos) {
     const curso = { codigo, nombre, creditos };
-    let { data, error } = await supabase.from('cursos').select('*')
+    let { error } = await supabase.from("cursos").insert([curso])
+
+    if (error) {
+        console.error(error);
+    }
+    cargarcursos();
 }
+async function eliminarcursos(id) {
+    let { error } = await supabase.from("cursos").delete().eq('id', id);
+    if (error) {
+        console.error(error);
+    }
+}
+
 cargarcursos();
