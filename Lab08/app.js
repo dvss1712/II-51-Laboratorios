5c30728c3a8d01ab651fbacc1a28223c979e3b82
David <dv1712@gmail.com>
2025-11-19 20:44:57 -0600

    lab8 v3

--- Lab08/app.js
+++ Lab08/app.js
@@ -1,5 +1,27 @@
 import { supabase } from './supabaseClient.js';
 
+const form = document.getElementById('curso-form');
+const inputid = document.getElementById('id');
+const inputcodigo = document.getElementById('codigo');
+const inputnombre = document.getElementById('nombre');
+const inputcreditos = document.getElementById('creditos');
+const btnSave = document.getElementById("btn-save");
+const btnCancel = document.getElementById("btn-cancel");
+const statusDiv = document.getElementById("status");
+ 
+// 
+// eventos
+//=========================
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const codigo = inputcodigo.value.trim();
+    const nombre = inputnombre.value.trim();
+    const creditos = parseInt(inputcreditos.value);
+    if (editando) {}
+    else {await crearcurso(codigo, nombre, creditos);}
+   
+});
+
 async function cargarcursos() {
     let { data: cursos, error } = await supabase.from('cursos').select('*');
     console.log(cursos);
@@ -12,9 +34,12 @@ async function cargarcursos() {
     listaCursos.innerHTML = '';
     cursos.forEach(curso => {
         let li = document.createElement('li');
-        li.textContent = `${curso.codigo}: ${curso.nombre}`;
+        li.textContent = `${curso.codigo} - ${curso.nombre} [${curso.creditos} créditos]`;
         lista.appendChild(li);
     });
 }
-
+async function crearcurso(codigo, nombre, creditos) {
+    const curso = { codigo, nombre, creditos };
+    let { data, error } = await supabase.from('cursos').select('*')
+}
 cargarcursos();
